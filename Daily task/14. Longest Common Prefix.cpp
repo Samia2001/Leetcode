e93d7dd8a8e027bb7f0270771894e11732765f9a
Samia Rahman <77110531+Samia2001@users.noreply.github.com>
2025-03-07 10:07:35 +0600

    Update 14. Longest Common Prefix.cpp

--- Daily task/14. Longest Common Prefix.cpp
+++ Daily task/14. Longest Common Prefix.cpp
@@ -1,1 +1,25 @@
 
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string match=strs[0];
+        for(int i=1; i<strs.size(); i++)
+        {
+           string p ="";
+           for(int j=0;j<strs[i].size();j++)
+           {
+                if(strs[i][j]==match[j])
+                {
+                    p=p+strs[i][j];
+                }
+                else
+                break;
+           }
+           
+           match=p;
+           if(match=="")break;
+        }
+    return match;
+        
+    }
+};
